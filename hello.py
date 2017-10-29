57b34eaa753d87b08765baa262ecd05bbe1c6704
zreox <izh1092@gmail.com>
2017-10-29 15:42:17 +0800

    Chapter 5: Shell context (5c)

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -47,6 +47,11 @@ class User(db.Model):
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+
+def make_shell_context():
+    return dict(app=app, db=db, Role=Role, User=User)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @app.errorhandler(404)
