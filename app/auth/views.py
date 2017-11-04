eeb2f9c70451995a90b3164ba19c54a5a13013db
zreox <izh1092@gmail.com>
2017-11-05 07:45:20 +0800

    Chapter 8: User registration (8d)

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
 from ..models import User
+from ..email import send_email
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -23,3 +25,16 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
