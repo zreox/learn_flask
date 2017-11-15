25585c122ce2e3b655249aaaa42d8cd442154f2a
zreox <izh1092@gmail.com>
2017-11-15 19:58:58 +0800

    Chapter 11: Blog posts (11a)

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 
 import os
 from app import create_app, db
-from app.models import Role, User, Permission
+from app.models import Role, User, Permission, Post
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -13,7 +13,8 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, Role=Role, User=User, Permission=Permission)
+    return dict(app=app, db=db, Role=Role, User=User, Permission=Permission,
+                Post=Post)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
