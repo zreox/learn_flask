35149332a91a468b82dd495dcb7972949658c924
zreox <izh1092@gmail.com>
2017-11-17 10:37:51 +0800

    Chapter 12: Database representation of followers (12a)

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 
 import os
 from app import create_app, db
-from app.models import Role, User, Permission, Post
+from app.models import Role, User, Follow, Permission, Post
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -13,8 +13,8 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, Role=Role, User=User, Permission=Permission,
-                Post=Post)
+    return dict(app=app, db=db, Role=Role, User=User, Follow=Follow,
+                Permission=Permission, Post=Post)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -2,7 +2,7 @@ import unittest
 import time
 from datetime import datetime
 from app import create_app, db
-from app.models import Permission, Role, User, AnonymousUser
+from app.models import Permission, Role, User, AnonymousUser, Follow
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -148,3 +148,40 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue('d=retro' in gravatar_retro)
         self.assertTrue('https://secure.gravatar.com/avatar/' +
                         '2335050b18fb0ffe388af9b4beab1c17' in gravatar_ssl)
+
+    def test_follows(self):
+        u1 = User(email='gon@example.com', password='dog')
+        u2 = User(email='kirua@example.com', password='fox')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        self.assertFalse(u1.is_following(u2))
+        self.assertFalse(u1.is_followed_by(u2))
+        timestamp_before = datetime.utcnow()
+        u1.follow(u2)
+        db.session.add(u1)
+        db.session.commit()
+        timestamp_after = datetime.utcnow()
+        self.assertTrue(u1.is_following(u2))
+        self.assertFalse(u1.is_followed_by(u2))
+        self.assertTrue(u2.is_followed_by(u1))
+        self.assertTrue(u1.followed.count() == 1)
+        self.assertTrue(u2.followers.count() == 1)
+        f = u1.followed.all()[-1]
+        self.assertTrue(f.followed == u2)
+        self.assertTrue(timestamp_before <= f.timestamp <= timestamp_after)
+        f = u2.followers.all()[-1]
+        self.assertTrue(f.follower == u1)
+        u1.unfollow(u2)
+        db.session.add(u1)
+        db.session.commit()
+        self.assertTrue(u1.followed.count() == 0)
+        self.assertTrue(u2.followers.count() == 0)
+        self.assertTrue(Follow.query.count() == 0)
+        u2.follow(u1)
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        db.session.delete(u2)
+        db.session.commit()
+        self.assertTrue(Follow.query.count() == 0)
