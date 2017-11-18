e552f0e9a8c043196cc39e6e3369356164b733f0
zreox <izh1092@gmail.com>
2017-11-18 11:00:49 +0800

    Chapter 13: Blog post comments (13a)

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 
 import os
 from app import create_app, db
-from app.models import Role, User, Follow, Permission, Post
+from app.models import Role, User, Follow, Permission, Post, Comment
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -14,7 +14,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, Role=Role, User=User, Follow=Follow,
-                Permission=Permission, Post=Post)
+                Permission=Permission, Post=Post, Comment=Comment)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
