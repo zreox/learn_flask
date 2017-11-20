9004b1fc010b6abe7c56de036665d18adb3c0487
zreox <izh1092@gmail.com>
2017-11-20 09:37:48 +0800

    Coverage metrics (15a)

--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+COV = None
+if os.environ.get('ZREOX_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 from app import create_app, db
 from app.models import Role, User, Follow, Permission, Post, Comment
 from flask_script import Manager, Shell
@@ -20,11 +26,28 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit test."""
+    if coverage and not os.environ.get('ZREOX_COVERAGE'):
+        import sys
+        import subprocess
+        os.environ['ZREOX_COVERAGE'] = '1'
+        # os.execvp(sys.executable, [sys.executable] + sys.argv)
+        sys.exit(subprocess.call([sys.executable] + sys.argv))
+
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print 'Coverage Summary:'
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print 'HTML version: file://%s/index.html' % covdir
+        COV.erase()
 
 
 if __name__ == '__main__':
